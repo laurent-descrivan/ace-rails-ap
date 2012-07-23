30baaa9ce5436ea8eeae1972aac795fafe31d730
Adrien Giboire <adrien.giboire@gmail.com>
2012-07-23 17:41:53 +0200

    FIX Add remote reference when updating and build source

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ext/ace && git pull && cd -
+cd ext/ace && git pull origin master && npm install && node Makefile.dryice.js -m -nc && cd -
 cp ext/ace/build/src/* vendor/assets/javascripts/
